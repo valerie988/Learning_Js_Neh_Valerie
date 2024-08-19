7a3b9964e747ef130193bf492b97cfc485940cad
valerie988 <neh16valerie@gmail.com>
2024-08-19 13:51:54 +0100

    Operator

--- Day_1/Excercise.js
+++ Day_1/Excercise.js
@@ -1,5 +1,31 @@
 "use strict"
-const MY_OBJECT = { key: "value" };
-MY_OBJECT.key = "otherValue";;
+console.log("Hello world")
+
+//variables a comment 
+let x = 5; //number 
+console.log(x);
+
+var name = "Ann" // string
+const PI = 3.14 
+const car = { carname: "toyota", carcolor: "black"}
+let flag = true // boolen
+
+// operators: AND OR NOT
+
+const temp = 50
+if(temp < 0){
+    console.log("It is cold");
+}
+else if(temp>0 && temp<=25){
+    console.log("It is warm");
+}else{
+    console.log("It is hot");
+}
 
 console.log("3+5");
+
+x = 5;
+while(x<10) {
+    x++
+}
+console.log(x)
