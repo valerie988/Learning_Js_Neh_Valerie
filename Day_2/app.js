6d30857ef9e4cb97da2a73bca64ea53910fa1289
valerie988 <neh16valerie@gmail.com>
2024-08-20 09:49:29 +0100

    return

--- Day_2/app.js
+++ Day_2/app.js
@@ -6,3 +6,11 @@ function add(a,b,c) {
 let result = add(2,5,9)  //function call or invocation
 console.log(result)
 
+//function call 
+let x = product(7,3)
+function product(p,q) {
+    return p*q //return value to x
+}
+console.log(x);
+
+
