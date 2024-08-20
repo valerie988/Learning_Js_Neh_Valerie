2e5ecdfd49cd28c3b9d71c4db839cd54c64d7c17
valerie988 <neh16valerie@gmail.com>
2024-08-20 11:33:49 +0100

    sum

--- Day_2/app.js
+++ Day_2/app.js
@@ -22,11 +22,22 @@ console.log("The temperature is ", temp(67), "celsuis"); //using function as var
 //function expression
 const X = function (a,b) { return a*b } //anonymous function
 
-//self-invoking function
+/*self-invoking function
 (function () {
     let x = "Hello"
-})();
+})();*/
 
 //arrow function
 const div = (a,b) => a/b
 console.log((10,2));
+
+//sum all numbers
+function sumAll() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+let res =sumAll(2,15,54,67)
+console.log("The sum of all the numbers is", res );
