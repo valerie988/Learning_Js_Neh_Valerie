09aeda2469847eef47608dd7bb525f4be3a27cc4
valerie988 <neh16valerie@gmail.com>
2024-08-21 12:02:59 +0100

    constructor

--- Day_3/array.js
+++ Day_3/array.js
@@ -17,3 +17,25 @@ animals.forEach((item, index) => {
     console.log(`${index}: ${item}`);
 });
 console.log(animals.reverse())
+
+animals[8] = "pig";
+let interator = animals.keys()
+for (const key of interator) {
+    console.log(`${key}: ${animals[key]}`)
+}
+
+const newAnimals = console.log(animals.sort())
+
+ let arr = [`a`,`b`,`c`,`d`,`e`];
+ console.log( arr.copyWithin(2,3))
+
+//creating an array using array() constructor
+const animals2 = new Array("fish", "rat", "elephant")
+console.log(animals2)
+
+//using string.prototype.Split()
+const animals3 = "donkey, lion, monkey".split(", ")
+console.log(animals3)
+
+//creating a string
+const animalString = animals.jion()
