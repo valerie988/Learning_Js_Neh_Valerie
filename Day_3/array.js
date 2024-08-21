fa531ff194cbf94802234cbb88402dacff10543c
valerie988 <neh16valerie@gmail.com>
2024-08-21 10:38:57 +0100

    reverse

--- Day_3/array.js
+++ Day_3/array.js
@@ -9,7 +9,11 @@ console.log("There are ", animals.length, "animals");
 console.log(Object.keys(animals));
 
 //reducing the length
-animals.length = 2;
+animals.length = 4;
 console.log(Object.keys(animals));
 console.log("There are ", animals.length, "animals");
 
+animals.forEach((item, index) => {
+    console.log(`${index}: ${item}`);
+});
+console.log(animals.reverse())
