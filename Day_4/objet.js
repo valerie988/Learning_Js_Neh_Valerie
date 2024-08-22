401fc3943bd3d403a04cdd07663339986fcb3c44
valerie988 <neh16valerie@gmail.com>
2024-08-22 09:15:59 +0100

    object properties

---
+++ Day_4/objet.js
@@ -0,0 +1,15 @@
+"use strict"
+
+//create an object
+const person = {
+    name: "Valerie",
+    age: 19,
+    level: 200,
+};
+ console.log(person)
+
+ //adding properties to an object
+const fruit = {}
+fruit.name = "banana";
+fruit.color = "yellow"
+console.log(fruit);
