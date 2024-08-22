69746a43f24811fc502e38946c6350242b2c03b0
valerie988 <neh16valerie@gmail.com>
2024-08-22 17:32:27 +0100

    Address book

--- Day_4/book.js
+++ Day_4/book.js
@@ -1,15 +1,32 @@
 "use strict"
 let Addressbook = [];
-function book(Name,phone,address,email){
+
+const submit = document.querySelector("button");
+
+function book(){
+    const name = document.querySelector("#name").value
+    const phone = document.querySelector("#phone").value
+    const address = document.querySelector("#address").value
+    const email = document.querySelector("#email").value
+    const contactList = document.querySelector("#contactList")
+
     const contact = {
-        Name: Name,
+        name: name,
         phone: phone,
         address: address,
         email: email,
     }
-    Addressbook.push(contact)
+    Addressbook.push(contact);
+    console.log(Addressbook);
+    
+
+    const contactElement = document.createElement("li")
+    Addressbook.forEach((contact,index) => {
+        contactElement.innerHTML = (`${contact.name}, ${contact.phone}, ${contact.address},${contact.email}`)
+        console.log(contactElement);
+    });
+    contactList.appendChild(contactElement);
 }
-const contact1 = {Name: "Valerie", phone: 652173171, address: "soa", email: "neh16ggmail.com"}
-
-console.log(book())
-
+submit.addEventListener(`click`, () => {
+    book()
+})
