b04e54d96b14fe86865784ad7f489271c64a8c99
valerie988 <neh16valerie@gmail.com>
2024-08-26 12:54:19 +0100

    callback

--- Day_6/react.js
+++ Day_6/react.js
@@ -7,6 +7,60 @@ function successCallback(result) {
 function errorCallback(error) {
     console.error(`Error generating audio file: ${error}`);
 }
-createAudioFileAsync(audioSettings, successCallback, errorCallback)
+//createAudioFileAsync(audioSettings, successCallback, errorCallback)
 
-createAudioFileAsync(audioSettings.then(successCallback, e))
+//createAudioFileAsync(audioSettings.then(successCallback, errorCallback))
+
+let a = 2
+let b = 5
+
+setTimeout(function () {
+    console.log(`async`);
+}, 10)
+
+console.log("synchronous")
+console.log(a);
+console.log(b);
+
+function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+}
+function myfirst() {
+    myDisplayer("Hello")
+    console.log("Hello");
+}
+function mySecond() {
+    myDisplayer("Goodbye")
+  console.log("Goodbye");
+}
+mySecond()
+myfirst()
+
+function calculator(num1, num2) {
+    let sum = num1  + num2
+    myDisplayer(sum)
+}
+calculator(4,7)
+
+function calculator(num1, num2, myCallback) {
+    let sum = num1 * num2
+    //myCallback(sum)
+}
+
+//calculator(4,6,myDisplayer)
+
+const myNum = [3,78,-211,6,-3,-18]
+const posNum = remNeg (myNum, (x) => { 
+    x>= 0
+})
+myDisplayer(posNum)
+function remNeg(num,callback) {
+    const myArray = []
+    for(const x in num){
+        if (callback(x)){
+            myArray.push(x)
+        }
+    }
+    return myArray;
+}
+remNeg()
