f9efbf21a40198b796f266a218ba724dcafd6633
valerie988 <neh16valerie@gmail.com>
2024-08-28 10:58:06 +0100

    chaining

--- Day_8/app.js
+++ Day_8/app.js
@@ -1,8 +1,9 @@
-const map = new Map()
+const map = new Map()  //creating a map
 map.set(1, "hello")
 map.set("1", "bye")
 map.set(true, "bool")
 
+//map properties
 console.log(map.get(1));
 console.log(map.get("1"));
 console.log(map.has(2));
@@ -11,7 +12,18 @@ console.log(del);
 
 console.log(map.size);
 
+//map as an object
 let person = { name: "valerie"}
 let position = new Map()
 position.set(person, 24)
 console.log(position.get(person));
+
+
+//chaining
+map.set(1, "hello")
+.set("1", "bye")
+.set(true, "bool")
+console.log(map);
+
+
+interating over maps
