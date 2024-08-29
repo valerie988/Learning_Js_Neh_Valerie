aaa38483ce4b8ac6edd19a9b1e8d3c3b31e047bf
valerie988 <neh16valerie@gmail.com>
2024-08-29 17:40:54 +0100

    done

--- Day_8/schedular.js
+++ Day_8/schedular.js
@@ -1,33 +1,46 @@
-let myArray = []
-const myObj = {
-    Name: "Name",
-    date: "dateline",
-    Priority: "priority",
-    state: "state"
-}
+document.addEventListener(`DOMContentLoaded`, () => {
+  const form = document.querySelector("#taskForm");
+  const activity = document.querySelector("#activity");
+  const message = document.querySelector("#message");
 
+  const tasknameSet = new Set();
+  const task = new Map();
 
-const form = document.querySelector("#taskForm")
+  form.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+    const taskname = document.querySelector(".task-name").value;
+    const taskdateline = document.querySelector(".task-dateline").value;
+    const taskpriority = document.querySelector(".task-priority").value;
+    const taskstate = document.querySelector(".task-state").value;
 
-form.addEventListener(`submit`, (e) => {
-    e.preventDefault()
-    const taskname = document.querySelector(".task-name").value
-    const taskdateline = document.querySelector(".task-dateline").value
-    const taskpriority = document.querySelector(".task-priority").value
-    const taskstate = document.querySelector(".task-state").value
+    console.log(tasknameSet);
+    console.log(taskname);
+    console.log(tasknameSet.has(taskname));
     
-    const task = {taskname, taskdateline, taskpriority, taskstate}
+    
+    if(tasknameSet.has(taskname)) {
+      message.textContent = "Task already exist";
+      message.style.color = "red";
+      console.log("entered");
+    } 
+    else {
+        tasknameSet.add(taskname);
+        task.set(taskname, {
+        taskdateline: "taskdateline",
+        taskpriority: "taskpriotity",
+        taskstate: "taskstate",
+      });
+    console.log("set");
 
-    myArray.push(task)
+    const activitylist = document.createElement("div");
 
-    const tasknameSet = new Set()
+    activitylist.innerHTML = `<h3>Task: ${taskname}</h3> <p>Dateline: ${taskdateline}</p> <p>Priority: ${taskpriority}</p> <p>State: ${taskstate}</p>`;
+    activity.appendChild(activitylist);
 
-    tasknameSet.add(myArray)
+    message.textContent = "Task added successfully!";
+    message.style.color = "green";
+    }
+      form.reset();
 
-    const activity = document.querySelector("#activity")
-    const activitylist = document.createElement("div")
-   
-    activitylist.innerHTML = `<h3>Task: ${taskname}</h3> <p>Dateline: ${taskdateline}</p> <p>Priority: ${taskpriority}</p> <p>State: ${taskstate}</p>`
-    activity.appendChild(activitylist)
-
-})
+  });
+});
